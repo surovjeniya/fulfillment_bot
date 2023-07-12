7caa51eb99a14d8ff601cd2ddc35eaa399aa32b6
surovjeniya <surovjeniya@gmail.com>
2023-07-12 12:03:56 +0300

    ...

--- app/src/sellershub-mailing-bot/command/command.module.ts
+++ app/src/sellershub-mailing-bot/command/command.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MailingBotUserEntity } from '../entity/mailing-bot-user.entity';
 import { SellersHubMailingBotUserService } from '../sellershub-mailing-bot.service';
 import { CaseService } from './case.service';
 import { CheckSubscribeForChannelService } from './check-subscribe-for-channel.service';
 import { CommandUpdate } from './command.update';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([MailingBotUserEntity])],
   providers: [
     CommandUpdate,
     CaseService,
